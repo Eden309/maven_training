8ebe5a950c9e90ff01431c52f2d821634533c784
Eden Loukakou-Bataille <loukakou-batai@et.esiea.fr>
2021-11-30 15:42:46 +0100

    Setup GitHub CI

---
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -0,0 +1,18 @@
+package fr.lernejo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @org.junit.jupiter.api.Test
+    void op() {
+        assertEquals(20, new Sample().op(Sample.Operation.ADD, 2, 18));
+        assertEquals(10, new Sample().op(Sample.Operation.MULT, 2, 5));
+    }
+
+    @org.junit.jupiter.api.Test
+    void fact() {
+        assertEquals(120, new Sample().fact(5));
+        assertEquals(720, new Sample().fact(6));
+    }
+}
